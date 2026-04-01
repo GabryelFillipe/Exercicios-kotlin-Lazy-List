0731fc253100d817b43986ca3fefc75a46f0485c
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@dsn127m1241955.cfp127aluno.local>
2026-04-01 11:27:15 -0300

    feat: adicinando uma lazy row

--- app/src/main/java/com/example/listas/MainActivity.kt
+++ app/src/main/java/com/example/listas/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -28,7 +32,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.listas.model.Game
@@ -60,7 +66,9 @@ fun GamesScreen(modifier: Modifier = Modifier) {
     var gamesList by remember { mutableStateOf(getAllGames()) }
 
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
@@ -87,6 +95,11 @@ fun GamesScreen(modifier: Modifier = Modifier) {
                 }
             }
         )
+        LazyRow() {
+            items(gamesList){
+                StudioCard(it)
+            }
+        }
         LazyColumn() {
             items (gamesList) {
                 GameCard(it)
@@ -104,7 +117,8 @@ fun GameCard(game: Game) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Column( modifier = Modifier.fillMaxWidth()
+            Column( modifier = Modifier
+                .fillMaxWidth()
                 .padding(16.dp)
                 .weight(3f)
             ) {
@@ -122,13 +136,30 @@ fun GameCard(game: Game) {
             }
 
             Text(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
                     .fillMaxWidth(),
                 text = "${game.releaseYear}",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Blue
             )
+        }
+    }
+}
+
+@Composable
+fun StudioCard(game: Game) {
+    Card(
+        modifier = Modifier.size(150.dp).padding(4.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+            Text(text = game.studio, textAlign = TextAlign.Center)
         }
     }
 }
